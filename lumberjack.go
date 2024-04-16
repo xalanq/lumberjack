660dcb08b325f79bd277c524d46d5f1d01eeae55
xalanq <xalanq@gmail.com>
2024-04-16 22:53:26 +0800

    upd: max size in bytes

--- lumberjack.go
+++ lumberjack.go
@@ -40,7 +40,7 @@ import (
 const (
 	backupTimeFormat = "2006-01-02T15-04-05.000"
 	compressSuffix   = ".gz"
-	defaultMaxSize   = 100
+	defaultMaxSize   = 100 * 1024 * 1024
 )
 
 // ensure we always implement io.WriteCloser
@@ -84,7 +84,7 @@ type Logger struct {
 	// os.TempDir() if empty.
 	Filename string `json:"filename" yaml:"filename"`
 
-	// MaxSize is the maximum size in megabytes of the log file before it gets
+	// MaxSize is the maximum size in bytes of the log file before it gets
 	// rotated. It defaults to 100 megabytes.
 	MaxSize int `json:"maxsize" yaml:"maxsize"`
 
@@ -123,11 +123,6 @@ var (
 
 	// os_Stat exists so it can be mocked out by tests.
 	osStat = os.Stat
-
-	// megabyte is the conversion factor between MaxSize and bytes.  It is a
-	// variable so tests can mock it out and not need to write megabytes of data
-	// to disk.
-	megabyte = 1024 * 1024
 )
 
 // Write implements io.Writer.  If a write would cause the log file to be larger
@@ -446,9 +441,9 @@ func (l *Logger) timeFromName(filename, prefix, ext string) (time.Time, error) {
 // max returns the maximum size in bytes of log files before rolling.
 func (l *Logger) max() int64 {
 	if l.MaxSize == 0 {
-		return int64(defaultMaxSize * megabyte)
+		return int64(defaultMaxSize)
 	}
-	return int64(l.MaxSize) * int64(megabyte)
+	return int64(l.MaxSize)
 }
 
 // dir returns the directory for the current filename.
